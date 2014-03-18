065dd5f18cd10ea056740f882d7604a8becf199d
anewball <andynewball@gmail.com>
2014-03-17 22:13:07 -0400

    Update main.cs

--- main.cs
+++ main.cs
@@ -2,27 +2,26 @@ using System;
 
 namespace Multiple
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{				
-			Console.Write("Please enter a number1: ");
-			int number1 = int.Parse(Console.ReadLine());
+   class Program
+   {
+     public static void Main(string[] args)
+     {				
+       Console.Write("Please enter number1: ");
+       int number1 = int.Parse(Console.ReadLine());
 			
-			Console.Write("\nPlease enter number2: ");
-			int number2 = int.Parse(Console.ReadLine());
+       Console.Write("\nPlease enter number2: ");
+       int number2 = int.Parse(Console.ReadLine());
 			
-			Console.WriteLine();
+       Console.WriteLine();
 			
-			if (number1 > number2)
-				Console.WriteLine("{0} is larger", number1);
-			else if(number2 > number1)
-				Console.WriteLine("{0} is larger", number2);
-			else
-				Console.WriteLine("These numbers are equal");
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
-		}
-	}
+       if (number1 > number2)     
+           Console.WriteLine("{0} is larger", number1);
+       else if(number2 > number1) 
+           Console.WriteLine("{0} is larger", number2);
+       else                       
+           Console.WriteLine("These numbers are equal");
+				
+       Console.ReadLine();
+     }
+   }
 }
